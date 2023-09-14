fdcc9b6d1bd3a732191d16bbf2b52a92c02779b7
ghanikhan1 <usmanatbit@gmail.com>
2023-09-14 21:37:30 +0100

    updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>Dojo Blog</h1>
-      <div className="links">
-        <a href="/">HOme</a>
+    <nav className="navbar p-20 flex items-center max-w-600  border border-b border-[#f2f2f2]">
+      <h1 className="text-[#f1356d]">Dojo Blog</h1>
+      <div className="links ml-auto">
+        <a className="ml-16 p-6 " href="/">HOme</a>
         <a href="/create">New Blog</a>
       </div>
     </nav>
