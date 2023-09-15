2494fca59b4681b21fa11e6fc620eeca723c6d7a
ghanikhan1 <usmanatbit@gmail.com>
2023-09-15 20:30:50 +0100

    message

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <h2>This is Home</h2>;
-};
+  const [blogs, setBlogs] = useState([
+    {title: 'my new website', body: 'lorem ipsum 1', author: 'Usman', id: 1},
+    {title: 'welcome', body: 'lorem ipsum 1', author: 'ghani', id: 2},
+    {title: 'development', body: 'lorem ipsum 1', author: 'khan', id: 3},
+  ]);
+
+  return (
+    <div className="home">
+    {blogs.map((blog) => (
+      <div className="blog-preview px-32 py-6 my-2 border border-b border-black " key={blog.id}>
+        <h2 className="text-red-400">{blog.title}</h2>
+        <p>Author: {blog.author}</p>
+      </div>
+    ))}
+
+    </div>
+  );
+}
 
 export default Home;
